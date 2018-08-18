24684f3a07f00b9c9f1c2e107c3f88eb77c37385
Kush <kushm66@gmail.com>
2018-08-19 04:13:12 +0530

    additions to be done in future

--- Python/Control.py
+++ Python/Control.py
@@ -1,7 +1,16 @@
 #Control File
+
 #Circulation Fan is switched on for 10mins and off for 2 
 #Exhaust is controlled by target temp and target humidity
 #Leds switch on for X hours a day in a length .
 #Air Pump switches on for 15 mins and off for 2.
 #Water Temperature reads, logs water data. Warns if temp is below or above limit.
+#Take Image every 10 mins.
+#Adjust thermostat(temp/humidity) as per the needs of plants. enter Targer-temp and target humidity to get started.
+#.. warn is temp/humidity is below or above the limits for plant survival
+
+#Log every on/off with time and date(no location)
+#Keep log of system UPTIME in hrs-mins
 #Add EC sensor, pH sensor 
+#Make a safe raspberry power off file
+#!!!! Where to show the Temp,Humidity, Image data ? Develop frontend
