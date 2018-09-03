87d1cce8ebbfa42900c0e932d5a4a9234d379d5d
Kush <kushm66@gmail.com>
2018-09-03 18:02:15 +0530

    update

--- Python/logData.py
+++ Python/logData.py
@@ -1,11 +1,14 @@
-from datetime import tzinfo, datetime,date
-#from time import gmtime
 import time
+from datetime import datetime
+import pytz
+
 counter=0
 def logData(name, status, attribute, value, comment):
-	timestamp = '{:%Y-%m-%d %H:%M:%S}'.format(datetime.utcnow())
+	location_india=pytz.timezone('Asia/Kolkata') #Enter your time Zone here
+	time_india=datetime.now(location_india)
+	time_stamp='{:%Y-%m-%d %H:%M:%S}'.format(time_india)
 	logFile(timestamp, name, status, attribute, value, comment)
-	logDB(timestamp, name, status, attribute, value, comment)
+	#logDB(timestamp, name, status, attribute, value, comment)
 
 def logFile(timestamp, name, status, attribute, value, comment):
     f = open('/home/pi/GYOF/Data/data.txt', 'a') #Edit File Path as per the name of the folder  i.e Repository
