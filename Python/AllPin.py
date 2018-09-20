3c2b4653b11b976ea38879d5dc42358255421bd4
Kush <kushm66@gmail.com>
2018-09-20 17:15:05 +0530

    Update Pin#

--- Python/AllPin.py
+++ Python/AllPin.py
@@ -1,13 +1,13 @@
 # Author: Saizenki
 # Date: 14.08.2018
-pina=7 # Fan Inside / Circulation
+pina=12 # Fan Inside / Circulation
 pinb=11 # Fan Outside/ Exhaust
 pinc=13 # LEDs
 pind=15 # AirPump
 pine=29  #DHT22
 pinSDA=3 #I2C
 pinSCL=5 #I2C
-#pinf=   #DS18B20
+#pinf= 7 #DS18B20
 pinf=31 #Optomax Digital Liquid Level Sensor EMPTY
 ping=33 #Optomax Digital Liquid Level Sensor FULL
 class Pin(object):
